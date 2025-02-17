7bdcce543b89ceef829a6eee705d143cd2d377cb
henk1Andrew <henk1Andrew@protonmail.com>
2025-02-16 22:56:40 -0700

    sync

--- profile-page/src/app/app.config.ts
+++ profile-page/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Material from '@primeng/themes/material';
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'})),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
